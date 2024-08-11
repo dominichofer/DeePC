40172130116f857e9dcd51aab6123570f1b4425f
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2024-08-12 01:40:06 +0200

    Extend interface of deePC and Controller

--- src/deepc/deepc.py
+++ src/deepc/deepc.py
@@ -5,27 +5,27 @@ from .math import hankel_matrix, projected_gradient_method
 
 
 def deePC(
-    _u_d: list[int | float],
-    _y_d: list[int | float],
-    _u_ini: list[int | float],
-    _y_ini: list[int | float],
-    _r: list[int | float],
+    u_d: list | np.ndarray,
+    y_d: list | np.ndarray,
+    u_ini: list | np.ndarray,
+    y_ini: list | np.ndarray,
+    r: list | np.ndarray,
     Q: np.ndarray | None = None,
     R: np.ndarray | None = None,
     control_constrain_fkt: Callable | None = None,
     max_pgm_iterations=300,
     pgm_tolerance=1e-6,
-) -> list[int | float]:
+) -> list[float]:
     """
     Returns the optimal control for a given system and reference trajectory.
     According to the paper Data-Enabled Predictive Control: In the Shallows of the DeePC
     https://arxiv.org/abs/1811.05890
     Args:
-        _u_d: Control inputs from an offline procedure.
-        _y_d: Outputs from an offline procedure.
-        _u_ini: Control inputs to initiate the state.
-        _y_ini: Outputs to initiate the state.
-        _r: Reference trajectory.
+        u_d: Control inputs from an offline procedure.
+        y_d: Outputs from an offline procedure.
+        u_ini: Control inputs to initiate the state.
+        y_ini: Outputs to initiate the state.
+        r: Reference trajectory.
         Q: Output cost matrix, defaults to identity matrix.
         R: Control cost matrix, defaults to zero matrix.
         control_constrain_fkt: Function that constrains the control.
@@ -33,27 +33,49 @@ def deePC(
                             used to solve the constrained optimization problem.
         pgm_tolerance: Tolerance for the PGM algorithm.
     """
-    assert len(_u_d) == len(_y_d), "u_d and y_d must have the same length."
-    assert len(_u_ini) == len(_y_ini), "u_ini and y_ini must have the same length."
+    u_d = np.array(u_d)
+    u_is_1d = u_d.ndim == 1
+    if u_d.ndim == 1:
+        u_d = u_d.reshape(-1, 1)
+    y_d = np.array(y_d)
+    if y_d.ndim == 1:
+        y_d = y_d.reshape(-1, 1)
+    u_ini = np.array(u_ini)
+    if u_ini.ndim == 1:
+        u_ini = u_ini.reshape(-1, 1)
+    y_ini = np.array(y_ini)
+    if y_ini.ndim == 1:
+        y_ini = y_ini.reshape(-1, 1)
+    r = np.array(r)
+    if r.ndim == 1:
+        r = r.reshape(-1, 1)
 
-    u_d = np.array(_u_d)
-    y_d = np.array(_y_d)
-    u_ini = np.array(_u_ini)
-    y_ini = np.array(_y_ini)
-    r = np.array(_r)
+    assert len(u_d) == len(y_d), "u_d and y_d must have the same length."
+    assert len(u_ini) == len(y_ini), "u_ini and y_ini must have the same length."
+    assert u_d.shape[1] == u_ini.shape[1], "Elements of u_d and u_ini must have the same dimension."
+    assert y_d.shape[1] == y_ini.shape[1] == r.shape[1], "Elements of y_d, y_ini, and r must have the same dimension."
+
     T_ini = len(u_ini)
+    r_len = len(r)
+    u_ndim = u_d.shape[1] if u_d.ndim == 2 else 1
+    y_ndim = y_d.shape[1] if y_d.ndim == 2 else 1
+
+    # Transform to column vectors
+    r = r.reshape(-1, 1)
+    u_ini = u_ini.reshape(-1, 1)
+    y_ini = y_ini.reshape(-1, 1)
 
     if Q is None:
-        Q = np.eye(len(r))
+        Q = np.eye(r_len * y_ndim)
     if R is None:
-        R = np.zeros((len(r), len(r)))
+        R = np.zeros((r_len * u_ndim, r_len * u_ndim))
 
-    U = hankel_matrix(T_ini + len(r), u_d)
-    U_p = U[:T_ini, :]  # past
-    U_f = U[T_ini:, :]  # future
-    Y = hankel_matrix(T_ini + len(r), y_d)
-    Y_p = Y[:T_ini, :]  # past
-    Y_f = Y[T_ini:, :]  # future
+    U = hankel_matrix(T_ini + r_len, u_d)
+    U_p = U[: T_ini * u_ndim, :]  # past
+    U_f = U[T_ini * u_ndim :, :]  # future
+    Y = hankel_matrix(T_ini + r_len, y_d)
+    Y_p = Y[: T_ini * y_ndim, :]  # past
+    Y_f = Y[T_ini * y_ndim :, :]  # future
 
     # Now solving
     # minimize: ||y - r||_Q^2 + ||u||_R^2
@@ -76,16 +98,9 @@ def deePC(
     # and get M * [x; u] = y.
 
     # We define [M_x; M_u] := M
-    M_x = M[:, : 2 * T_ini]
-    M_u = M[:, 2 * T_ini :]
-    # to get M_u_ini * u_ini + M_y_ini * y_ini + M_u * u = y.
-
-    # TODO: Delete this!
-    # B = np.zeros_like(M_u_ini)
-    # B[:, : M_u.shape[1]] = M_u
-    # u_bar = np.linalg.solve(B + M_u_ini, r - M_y_ini @ r[:T_ini])
-    # if not np.allclose(M_u_ini @ u_bar[:T_ini] + M_y_ini @ r[:T_ini] + M_u @ u_bar, r):
-    #     raise ValueError("The solution is not correct.")
+    # such that M_x * x + M_u * u = y.
+    M_x = M[:, : len(x)]
+    M_u = M[:, len(x) :]
 
     # We can now solve the unconstrained problem.
     # This is a ridge regression problem with generalized Tikhonov regularization.
@@ -98,21 +113,24 @@ def deePC(
     u_star = np.linalg.solve(G, w)
 
     if control_constrain_fkt is not None:
-        u_star = projected_gradient_method(
-            G, u_star, w, control_constrain_fkt, max_pgm_iterations, pgm_tolerance
-        )
-    return u_star[:, 0].tolist()
+        u_star = projected_gradient_method(G, u_star, w, control_constrain_fkt, max_pgm_iterations, pgm_tolerance)
+
+    if u_is_1d:
+        u_star = u_star[:, 0]
+    else:
+        u_star = u_star.reshape(-1, u_d.shape[1])
+    return u_star.tolist()
 
 
 class Controller:
     def __init__(
         self,
-        _u_d: list[int | float],
-        _y_d: list[int | float],
+        u_d: list | np.ndarray,
+        y_d: list | np.ndarray,
         T_ini: int,
         r_len: int,
-        u_ini: list[int | float] | None = None,
-        y_ini: list[int | float] | None = None,
+        u_ini: list | np.ndarray | None = None,
+        y_ini: list | np.ndarray | None = None,
         Q: np.ndarray | None = None,
         R: np.ndarray | None = None,
         control_constrain_fkt: Callable | None = None,
@@ -136,29 +154,43 @@ class Controller:
                                 used to solve the constrained optimization problem.
             pgm_tolerance: Tolerance for the PGM algorithm.
         """
-        u_d = np.array(_u_d)
-        y_d = np.array(_y_d)
-
         assert len(u_d) == len(y_d), "u_d and y_d must have the same length."
         assert T_ini > 0, "T_ini must be greater than zero."
         assert (u_ini is None) == (y_ini is None), "u_ini and y_ini must be both None or not None."
         assert u_ini is None or len(u_ini) == T_ini, "u_ini must have the same length as T_ini."
         assert y_ini is None or len(y_ini) == T_ini, "y_ini must have the same length as T_ini."
 
+        u_d = np.array(u_d)
+        self.u_is_1d = u_d.ndim == 1
+        if u_d.ndim == 1:
+            u_d = u_d.reshape(-1, 1)
+        y_d = np.array(y_d)
+        if y_d.ndim == 1:
+            y_d = y_d.reshape(-1, 1)
+
+        u_ndim = u_d.shape[1] if u_d.ndim == 2 else 1
+        y_ndim = y_d.shape[1] if y_d.ndim == 2 else 1
+
         self.T_ini = T_ini
         self.r_len = r_len
         self.u_ini: collections.deque[np.ndarray] = collections.deque(maxlen=T_ini)
         if u_ini is not None:
+            u_ini = np.array(u_ini)
+            if u_ini.ndim == 1:
+                u_ini = u_ini.reshape(-1, 1)
             for u in u_ini:
                 self.u_ini.append(np.array(u))
         self.y_ini: collections.deque[np.ndarray] = collections.deque(maxlen=T_ini)
         if y_ini is not None:
+            y_ini = np.array(y_ini)
+            if y_ini.ndim == 1:
+                y_ini = y_ini.reshape(-1, 1)
             for y in y_ini:
                 self.y_ini.append(np.array(y))
         if Q is None:
-            Q = np.eye(r_len)
+            Q = np.eye(r_len * y_ndim)
         if R is None:
-            R = np.zeros((r_len, r_len))
+            R = np.zeros((r_len * u_ndim, r_len * u_ndim))
         self.Q = Q
         self.R = R
         self.control_constrain_fkt = control_constrain_fkt
@@ -166,11 +198,11 @@ class Controller:
         self.pgm_tolerance = pgm_tolerance
 
         U = hankel_matrix(T_ini + r_len, u_d)
-        U_p = U[:T_ini, :]  # past
-        U_f = U[T_ini:, :]  # future
+        U_p = U[: T_ini * u_ndim, :]  # past
+        U_f = U[T_ini * u_ndim :, :]  # future
         Y = hankel_matrix(T_ini + r_len, y_d)
-        Y_p = Y[:T_ini, :]  # past
-        Y_f = Y[T_ini:, :]  # future
+        Y_p = Y[: T_ini * y_ndim, :]  # past
+        Y_f = Y[T_ini * y_ndim :, :]  # future
 
         # Now solving
         # minimize: ||y - r||_Q^2 + ||u||_R^2
@@ -193,8 +225,8 @@ class Controller:
         # and get M * [x; u] = y.
 
         # We define (M_x, M_u) := M such that M_x * x + M_u * u = y.
-        self.M_x = M[:, : 2 * T_ini]
-        self.M_u = M[:, 2 * T_ini :]
+        self.M_x = M[:, : T_ini * (u_ndim + y_ndim)]
+        self.M_u = M[:, T_ini * (u_ndim + y_ndim) :]
 
         # We can now solve the unconstrained problem.
         # This is a ridge regression problem with generalized Tikhonov regularization.
@@ -210,7 +242,7 @@ class Controller:
         "Returns whether the internal state is initialized."
         return len(self.u_ini) == self.T_ini and len(self.y_ini) == self.T_ini
 
-    def update(self, u: list[int | float], y: list[int | float]) -> None:
+    def update(self, u: list | np.ndarray, y: list | np.ndarray) -> None:
         "Updates the internal state with the given control input and trajectory."
         self.u_ini.append(np.array(u))
         self.y_ini.append(np.array(y))
@@ -220,16 +252,18 @@ class Controller:
         self.u_ini.clear()
         self.y_ini.clear()
 
-    def apply(self, _r: list[int | float]) -> list[int | float]:
+    def apply(self, r: list | np.ndarray) -> list[float]:
         """
         Returns the optimal control for a given reference trajectory.
         Args:
             r: Reference trajectory.
         """
         assert self.is_initialized(), "Internal state is not initialized."
-        assert len(_r) == self.r_len, "Reference trajectory has wrong length."
+        assert len(r) == self.r_len, "Reference trajectory has wrong length."
 
-        r = np.array(_r).reshape(-1, 1)
+        # Transform to column vectors
+        r = np.array(r).reshape(-1, 1)
+
         x = np.concatenate([self.u_ini, self.y_ini]).reshape(-1, 1)
         w = self.M_u.T @ self.Q @ (r - self.M_x @ x)
         u_star = np.linalg.solve(self.G, w)
--- tests/deepc/test_deepc.py
+++ tests/deepc/test_deepc.py
@@ -1,9 +1,9 @@
 import unittest
 import numpy as np
-from deepc import clamp, linear_chirp, deePC, Controller, DiscreteLTI, LaggedLTI
+from deepc import clamp, linear_chirp, deePC, Controller, DiscreteLTI
 
 
-def lti_1D_to_1D() -> DiscreteLTI:
+def lti_1D_input_1D_output() -> DiscreteLTI:
     "LTI system with 1D input and 1D output"
     system = DiscreteLTI(
         A=[[0.9, -0.2], [0.7, 0.1]],
@@ -12,21 +12,25 @@ def lti_1D_to_1D() -> DiscreteLTI:
         D=[[0.1]],
         x_ini=[1, 1],
     )
+    assert system.input_dim == 1
+    assert system.output_dim == 1
     assert system.is_controllable()
     assert system.is_observable()
     assert system.is_stable()
     return system
 
 
-def lti_3D_to_3D() -> DiscreteLTI:
-    "LTI system with 3D input and 3D output"
+def lti_2D_input_3D_output() -> DiscreteLTI:
+    "LTI system with 3D input and 2D output"
     system = DiscreteLTI(
         A=[[0.5, 0.1, 0], [0.1, 0.5, 0.1], [0, 0.1, 0.5]],
-        B=[[0.1, 0, 0], [0, 0.1, 0], [0, 0, 0.1]],
-        C=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
-        D=[[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+        B=[[0.1, 0], [0.1, 0.5], [0, 0.1]],
+        C=[[1, 0, 0], [0, 1, 1], [0, 0, 1]],
+        D=[[0, 0], [0, 0], [0, 0]],
         x_ini=[0, 0, 0],
     )
+    assert system.input_dim == 2
+    assert system.output_dim == 3
     assert system.is_controllable()
     assert system.is_observable()
     assert system.is_stable()
@@ -34,18 +38,243 @@ def lti_3D_to_3D() -> DiscreteLTI:
 
 
 def gather_offline_data(system: DiscreteLTI) -> tuple:
-    u_d = linear_chirp(0, 500, 1000)
+    if system.input_dim == 1:
+        u_d = linear_chirp(0, 500, 1000)
+    else:
+        chirps = [linear_chirp(0, 500, 1000, 0.1 * i) for i in range(system.input_dim)]
+        u_d = list(zip(*chirps))  # Transpose the list of chirp signals
     y_d = system.apply_multiple(u_d)
     return u_d, y_d
 
 
+class Test_deePC_1D_input_1D_output(unittest.TestCase):
+    def setUp(self):
+        self.system = lti_1D_input_1D_output()
+
+        # Offline data
+        self.u_d, self.y_d = gather_offline_data(self.system)
+
+        # Initial conditions
+        self.u_ini = [1] * 20
+        self.y_ini = self.system.apply_multiple(self.u_ini)
+
+        # Reference trajectory
+        self.r = [3, 3]
+
+    def test_unconstrained(self):
+        u_star = deePC(self.u_d, self.y_d, self.u_ini, self.y_ini, self.r)
+
+        y_star = self.system.apply_multiple(u_star)
+        np.testing.assert_array_almost_equal(y_star, self.r)
+
+    def test_constrained(self):
+        u_star = deePC(
+            self.u_d,
+            self.y_d,
+            self.u_ini,
+            self.y_ini,
+            self.r,
+            control_constrain_fkt=lambda u: clamp(u, -15, 15),
+        )
+
+        y_star = self.system.apply_multiple(u_star)
+        np.testing.assert_array_almost_equal(y_star, self.r)
+
+
+class Test_deePC_2D_input_3D_output(unittest.TestCase):
+    def setUp(self):
+        self.system = lti_2D_input_3D_output()
+
+        # Offline data
+        self.u_d, self.y_d = gather_offline_data(self.system)
+
+        # Initial conditions
+        self.u_ini = [(1, 1)] * 20
+        self.y_ini = self.system.apply_multiple(self.u_ini)
+
+        # Reference trajectory
+        self.r = [(0.21, 0.9, 0.36)]
+
+    def test_unconstrained(self):
+        u_star = deePC(self.u_d, self.y_d, self.u_ini, self.y_ini, self.r)
+
+        y_star = self.system.apply_multiple(u_star)
+        np.testing.assert_array_almost_equal(y_star, self.r, decimal=2)
+
+    def test_constrained(self):
+        u_star = deePC(
+            self.u_d,
+            self.y_d,
+            self.u_ini,
+            self.y_ini,
+            self.r,
+            control_constrain_fkt=lambda u: clamp(u, -15, 15),
+        )
+
+        y_star = self.system.apply_multiple(u_star)
+        np.testing.assert_array_almost_equal(y_star, self.r, decimal=2)
+
+
+class deePC_simple_system_1D_input_1D_output(unittest.TestCase):
+    def test_int(self):
+        u_d = [1, 2, 3, 4, -5, 6, 7, 8, 9, 10]
+        y_d = [1, 2, 3, 4, -5, 6, 7, 8, 9, 10]
+        u_ini = [2]
+        y_ini = [2]
+        u = [4]
+        r = [4]
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+    def test_int_with_2_targets(self):
+        u_d = [1, 2, 3, 4, -5, 6, 7, 8, 9, 10]
+        y_d = [1, 2, 3, 4, -5, 6, 7, 8, 9, 10]
+        u_ini = [2]
+        y_ini = [2]
+        u = [4, 4]
+        r = [4, 4]
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+    def test_tuple(self):
+        u_d = [(1,), (2,), (3,), (4,), (5,)]
+        y_d = [(1,), (2,), (3,), (4,), (5,)]
+        u_ini = [(2,)]
+        y_ini = [(2,)]
+        u = [(4,)]
+        r = [(4,)]
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+    def test_tuple_with_2_targets(self):
+        u_d = [(1,), (2,), (3,), (4,), (-5,), (6,), (7,), (8,), (9,), (10,)]
+        y_d = [(1,), (2,), (3,), (4,), (-5,), (6,), (7,), (8,), (9,), (10,)]
+        u_ini = [(2,)]
+        y_ini = [(2,)]
+        u = [(4,), (4,)]
+        r = [(4,), (4,)]
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+    def test_list(self):
+        u_d = [[1], [2], [3], [4], [5]]
+        y_d = [[1], [2], [3], [4], [5]]
+        u_ini = [[2]]
+        y_ini = [[2]]
+        u = [[4]]
+        r = [[4]]
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+    def test_list_with_3_targets(self):
+        u_d = [[1], [2], [3], [4], [-5], [6], [7], [8], [9], [10]]
+        y_d = [[1], [2], [3], [4], [-5], [6], [7], [8], [9], [10]]
+        u_ini = [[2]]
+        y_ini = [[2]]
+        u = [[4], [4], [4]]
+        r = [[4], [4], [4]]
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+    def test_list_of_nparray(self):
+        u_d = [np.array([1]), np.array([2]), np.array([3]), np.array([4]), np.array([5])]
+        y_d = [np.array([1]), np.array([2]), np.array([3]), np.array([4]), np.array([5])]
+        u_ini = [np.array([2])]
+        y_ini = [np.array([2])]
+        u = [np.array([4])]
+        r = [np.array([4])]
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+    def test_nparray(self):
+        u_d = np.array([1, 2, 3, 4, 5])
+        y_d = np.array([1, 2, 3, 4, 5])
+        u_ini = np.array([2])
+        y_ini = np.array([2])
+        u = np.array([4])
+        r = np.array([4])
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+
+class deePC_simple_system_2D_input_3D_output(unittest.TestCase):
+    def test_tuple(self):
+        u_d = [(x, y) for x in range(3) for y in range(3)]
+        y_d = [(x, y, x + y) for x, y in u_d]
+        u_ini = [(2, 2)]
+        y_ini = [(x, y, x + y) for x, y in u_ini]
+        u = [(1, 3)]
+        r = [(x, y, x + y) for x, y in u]
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+    def test_list(self):
+        u_d = [[x, y] for x in range(3) for y in range(3)]
+        y_d = [[x, y, x + y] for x, y in u_d]
+        u_ini = [[2, 2]]
+        y_ini = [[x, y, x + y] for x, y in u_ini]
+        u = [[1, 3]]
+        r = [[x, y, x + y] for x, y in u]
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+    def test_list_of_nparray(self):
+        u_d = [np.array([x, y]) for x in range(3) for y in range(3)]
+        y_d = [np.array([x, y, x + y]) for x, y in u_d]
+        u_ini = [np.array([2, 2])]
+        y_ini = [np.array([x, y, x + y]) for x, y in u_ini]
+        u = [np.array([1, 3])]
+        r = [np.array([x, y, x + y]) for x, y in u]
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+    def test_nparray(self):
+        u_d = np.array([[x, y] for x in range(3) for y in range(3)])
+        y_d = np.array([[x, y, x + y] for x, y in u_d])
+        u_ini = np.array([[2, 2]])
+        y_ini = np.array([[x, y, x + y] for x, y in u_ini])
+        u = np.array([[1, 3]])
+        r = np.array([[x, y, x + y] for x, y in u])
+
+        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
+
+        np.testing.assert_array_almost_equal(u_star, u)
+
+
 def warm_up_controller(controller: Controller, system: DiscreteLTI, u: float) -> None:
+    "Warm up the controller until it is initialized"
     while not controller.is_initialized():
         y = system.apply(u)
         controller.update(u, y)
 
 
 def control_system(controller: Controller, system: DiscreteLTI, r: list, time_steps: int) -> float:
+    """
+    Control the system for a given number of time steps.
+    Returns the output of the system after the last time step.
+    """
     for _ in range(time_steps):
         u = controller.apply(r)[0]
         y = system.apply(u)
@@ -53,106 +282,29 @@ def control_system(controller: Controller, system: DiscreteLTI, r: list, time_st
     return y
 
 
-class TestDeePC(unittest.TestCase):
-    def test_unconstrained_lag_0(self):
-        system = LaggedLTI(lag=0, x_ini=[1])
-
-        # Offline data
-        u_d = [1, 2, 3, 4, 5]
-        y_d = system.apply_multiple(u_d)
-
-        # Initial conditions
-        u_ini = [2]
-        y_ini = system.apply_multiple(u_ini)
-
-        # Reference trajectory
-        r = [4]
-
-        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
-
-        y_star = system.apply(u_star)
-        np.testing.assert_array_almost_equal(y_star, r)
-
-    def test_unconstrained_2D_LTI(self):
-        system = lti_1D_to_1D()
-
-        # Offline data
-        u_d, y_d = gather_offline_data(system)
-
-        # Initial conditions
-        u_ini = [1] * 20
-        y_ini = system.apply_multiple(u_ini)
-
-        # Reference trajectory
-        r = [3, 3]
-
-        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
-
-        y_star = system.apply_multiple(u_star)
-        self.assertAlmostEqual(y_star[0], r[0])
-
-    def test_constrained_2D_LTI(self):
-        system = lti_1D_to_1D()
-
-        # Offline data
-        u_d, y_d = gather_offline_data(system)
-
-        # Initial conditions
-        u_ini = [1] * 20
-        y_ini = system.apply_multiple(u_ini)
-
-        # Reference trajectory
-        r = [3, 3]
-
-        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
-
-        y_star = system.apply_multiple(u_star)
-        self.assertAlmostEqual(y_star[0], r[0])
-
-    def test_constrained_LTI_3D_to_3D(self):
-        system = lti_3D_to_3D()
-
-        # Offline data
-        u_d, y_d = gather_offline_data(system)
-
-        # Initial conditions
-        u_ini = [1] * 20
-        y_ini = system.apply_multiple(u_ini)
-
-        # Reference trajectory
-        r = [3, 3]
-
-        u_star = deePC(u_d, y_d, u_ini, y_ini, r)
-
-        y_star = system.apply_multiple(u_star)
-        self.assertAlmostEqual(y_star[0], r[0])
-
-
 class TestController(unittest.TestCase):
     def test_unconstrained_2D_LTI(self):
-        system = lti_1D_to_1D()
+        system = lti_1D_input_1D_output()
         u_d, y_d = gather_offline_data(system)
         T_ini = 20
         r_len = 3
         r = [10] * r_len
 
         controller = Controller(u_d, y_d, T_ini, r_len)
-        warm_up_controller(controller, system, 1)
+        warm_up_controller(controller, system, u=1)
         y = control_system(controller, system, r, time_steps=2 * T_ini)
 
         self.assertAlmostEqual(y, r[0])
 
     def test_constrained_2D_LTI(self):
-        system = lti_1D_to_1D()
+        system = lti_1D_input_1D_output()
         u_d, y_d = gather_offline_data(system)
         T_ini = 20
         r_len = 1
         r = [10] * r_len
 
-        controller = Controller(
-            u_d, y_d, T_ini, r_len, control_constrain_fkt=lambda u: clamp(u, 0, 25)
-        )
-        warm_up_controller(controller, system, 1)
+        controller = Controller(u_d, y_d, T_ini, r_len, control_constrain_fkt=lambda u: clamp(u, 0, 25))
+        warm_up_controller(controller, system, u=1)
         y = control_system(controller, system, r, time_steps=2 * T_ini)
 
         self.assertAlmostEqual(y, r[0])
