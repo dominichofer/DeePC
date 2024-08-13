696f5b869dc037ad011c2121129b90f01cca3fd7
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2024-08-13 02:47:48 +0200

    Rename RandomNoisyLTI to RandomNoiseDiscreteLTI

--- src/deepc/__init__.py
+++ src/deepc/__init__.py
@@ -1,5 +1,5 @@
 from .math import linear_chirp
-from .lti import DiscreteLTI, RandomNoisyLTI
+from .lti import DiscreteLTI, RandomNoiseDiscreteLTI
 from .deepc import deePC, Controller
 
-__all__ = ["linear_chirp", "DiscreteLTI", "RandomNoisyLTI", "deePC", "Controller"]
+__all__ = ["linear_chirp", "DiscreteLTI", "RandomNoiseDiscreteLTI", "deePC", "Controller"]
