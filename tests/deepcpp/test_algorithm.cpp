400d92abf5d2591ffa1296a2060155e8ad8a5914
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2024-08-19 19:59:25 +0200

    Fix to_string test

--- tests/deepcpp/test_algorithm.cpp
+++ tests/deepcpp/test_algorithm.cpp
@@ -10,7 +10,7 @@ TEST(Algorithm, To_String)
 	auto v = Vectors({1, 2}, {3, 4});
 	auto res = to_string(v);
 
-	EXPECT_EQ(res, "[[1, 2][3, 4]]");
+	EXPECT_EQ(res, "[[1.000000, 2.000000][3.000000, 4.000000]]");
 }
 
 TEST(Algorithm, Clamp)
