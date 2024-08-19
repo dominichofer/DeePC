cf1a77a9ad00d01fc8bb05f5fe020563ac22868a
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2024-08-19 02:48:20 +0200

    Simplify lti tests

--- tests/deepc/test_lti.py
+++ tests/deepc/test_lti.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 from deepc.lti import DiscreteLTI
 
 
@@ -41,7 +42,7 @@ class TestDiscreteLTI(unittest.TestCase):
             B=[[0], [0]],
             C=[[1, 0]],
             D=[[0]],
-            x_ini=[1, 1],
+            x_ini=[0, 0],
         )
         self.assertFalse(system.is_controllable())
 
@@ -51,7 +52,7 @@ class TestDiscreteLTI(unittest.TestCase):
             B=[[1], [0]],
             C=[[1, 0]],
             D=[[0]],
-            x_ini=[1, 1],
+            x_ini=[0, 0],
         )
         self.assertTrue(system.is_observable())
 
@@ -61,7 +62,7 @@ class TestDiscreteLTI(unittest.TestCase):
             B=[[1], [0]],
             C=[[1, 0]],
             D=[[0]],
-            x_ini=[1, 1],
+            x_ini=[0, 0],
         )
         self.assertFalse(system.is_observable())
 
@@ -71,7 +72,7 @@ class TestDiscreteLTI(unittest.TestCase):
             B=[[1], [1]],
             C=[[1, 0]],
             D=[[0]],
-            x_ini=[1, 1],
+            x_ini=[0, 0],
         )
         self.assertTrue(system.is_stable())
 
@@ -81,43 +82,53 @@ class TestDiscreteLTI(unittest.TestCase):
             B=[[1], [1]],
             C=[[1, 0]],
             D=[[0]],
-            x_ini=[1, 1],
+            x_ini=[0, 0],
         )
         self.assertFalse(system.is_stable())
 
-    def test_apply(self):
+    def test_apply_1d(self):
         system = DiscreteLTI(
-            A=[[0.5, 0], [0.6, 0.1]],
-            B=[[0.1], [1]],
-            C=[[1, 0]],
-            D=[[0.5]],
+            A=[[1, 1], [1, 1]],
+            B=[[1], [1]],
+            C=[[1, 1]],
+            D=[[1]],
             x_ini=[1, 1],
         )
         y = system.apply(1)
-        self.assertAlmostEqual(y, 1.1)
+        self.assertAlmostEqual(y, 7)
 
-    def test_apply_multiple(self):
+    def test_apply_2d(self):
         system = DiscreteLTI(
-            A=[[0.5, 0], [0.6, 0.1]],
-            B=[[0.1], [1]],
-            C=[[1, 0]],
-            D=[[0.5]],
+            A=[[1, 1], [1, 1]],
+            B=[[1, 1], [1, 1]],
+            C=[[1, 1]],
+            D=[[1, 1]],
+            x_ini=[1, 1],
+        )
+        y = system.apply([1, 1])
+        self.assertAlmostEqual(y, 10)
+
+    def test_apply_multiple_1d(self):
+        system = DiscreteLTI(
+            A=[[1, 1], [1, 1]],
+            B=[[1], [1]],
+            C=[[1, 1]],
+            D=[[1]],
             x_ini=[1, 1],
         )
         y = system.apply_multiple([1, 2, 3])
-        expected = [1.1, 1.5, 2.05]
-        self.assertAlmostEqual(y, expected)
+        np.testing.assert_almost_equal(y, [7, 18, 41])
 
-    def test_apply_control_2d(self):
+    def test_apply_multiple_2d(self):
         system = DiscreteLTI(
-            A=[[0.5, 0], [0.6, 0.1]],
-            B=[[1, 0.1], [0.1, 1]],
-            C=[[1, 0]],
-            D=[[0.5, 0]],
+            A=[[1, 1], [1, 1]],
+            B=[[1, 1], [1, 1]],
+            C=[[1, 1]],
+            D=[[1, 1]],
             x_ini=[1, 1],
         )
-        y = system.apply([1, 2])
-        self.assertAlmostEqual(y, 2.2)
+        y = system.apply_multiple([[1, 1], [2, 2]])
+        np.testing.assert_almost_equal(y, [10, 28])
 
 
 if __name__ == "__main__":
