985f331b059e383d7b956581b975a562a94f8fda
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2025-02-12 23:16:13 +0100

    Remove linear_chirp

--- src/deepc/__init__.py
+++ src/deepc/__init__.py
@@ -1,6 +1,5 @@
-from .math import linear_chirp
 from .lti import DiscreteLTI, RandomNoiseDiscreteLTI
 from .deepc import deePC
 from .controller import Controller
 
-__all__ = ["linear_chirp", "DiscreteLTI", "RandomNoiseDiscreteLTI", "deePC", "Controller"]
+__all__ = ["DiscreteLTI", "RandomNoiseDiscreteLTI", "deePC", "Controller"]
